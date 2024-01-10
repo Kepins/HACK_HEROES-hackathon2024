9cd0992f28aac36f768a41b2cafe09957390ab63
Szymon Kępiński <70029368+Kepins@users.noreply.github.com>
2024-01-10 23:26:38 +0100

    Shortest path (#9)
    * astar
    
    * Add doors and fix ASTAR
    
    * requirements.txt

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from draw_on_map import Point, PathDrawer
 from hall_factory import generate_nodes
+from models import Graph
 from magazine_map import MagazineMap
-from models import Node, Edge
 
 
 def main():
-    nodes = generate_nodes()
+    nodes, doors = generate_nodes()
+    graph = Graph(nodes=nodes, doors=doors)
     magazine = MagazineMap(nodes=nodes, csv_path='TASK-FILES/ProductsLokalization.csv')
+
+    path = graph.shortest_path(start=graph.nodes[217], end=graph.nodes[35])
+    p = PathDrawer(
+        real_points=[Point(n.x, n.y) for n in path])
+    p.draw_path()
+
 
 if __name__ == '__main__':
     main()
