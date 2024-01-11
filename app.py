9df43a651d15bd580be2649d5e94a95972e77a70
ola567 <aleksandra.hein@o2.pl>
2024-01-11 02:32:25 +0100

    web_app

--- app.py
+++ app.py
@@ -47,18 +47,13 @@ def paths():
                 db.add(path_obj)
                 db.commit()
 
-
                 path = graph.shortest_path(start=graph.nodes[217], end=magazine.get_node(row["ID Produktu"]))
-
                 png_path = os.path.join("static", str(tour.id), f"{path_obj.id}.png")
-
                 path_drawer.draw_path(real_points=[Point(n.x, n.y) for n in path], output_image_path=png_path)
 
-                path_obj.path_to_png = png_path
+                path_obj.path_to_png = f'/{png_path}'
                 db.add(path_obj)
                 db.commit()
-
-
 
             return redirect("/paths/" + str(tour.id))
     return render_template('upload_form.html')
@@ -66,9 +61,8 @@ def paths():
 
 @app.route('/paths/<id>', methods=['GET'])
 def pathsid(id):
-    tour = db.query(Tour).filter(Tour.id == id)
-
-    return render_template('index.html')
+    paths_files = db.query(Path.path_to_png).filter(Path.tour_id == id).all()
+    return render_template('index.html', paths_files=paths_files)
 
 
 if __name__ == '__main__':
